e2a95b570332b8ceba5e6859a5f968c71460e683
Pete Peterson <petersonpf@ornl.gov>
2015-11-30 15:42:43 -0500

    Adding license to setup

--- setup.py
+++ setup.py
@@ -5,18 +5,19 @@
 # the windows installer and the nxs directory copied to
 # the python site-packages directory
 #
-# On Linux the Makefile will also install python, but not 
+# On Linux the Makefile will also install python, but not
 # using this file - instead it uses the automake python install bits
 #
 # To use this file type:
 #
 #        python setup.py install
 #
-from distutils.core import setup 
- 
-setup(name='NeXus', 
-       version='4.2', 
-       description='Python Bindings to libNeXus', 
-       author='Paul Kienzle', 
-       packages = ['nxs'], 
-       ) 
+from distutils.core import setup
+
+setup(name='NeXus',
+      version='4.2',
+      description='Python Bindings to libNeXus',
+      author='Paul Kienzle',
+      packages = ['nxs'],
+      license='LGPLv2+'
+)
